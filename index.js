5c106b663f65d2b28a335aec56b5cf8f8adabac0
walid <walid.bouguima@gmail.com>
2020-04-27 00:29:27 +0200

    Solving the problem of Callback Hell by implementing Promises and chaining .then() in the operations file and the index.js as well

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const MongoClient = require('mongodb').MongoClient; 
 const assert = require('assert'); 
 const dboper = require('./operations');
-
-
 const url = 'mongodb://localhost:27017/'; 
 const dbname = 'conFusion'; 
 
-MongoClient.connect(url, (err, client) => {
-    assert.equal(err, null); 
+MongoClient.connect(url).then((client) => {
+    
     console.log('Connected correctly to server'); 
     const db = client.db(dbname); 
 
-    dboper.insertDocument(db, {name: "Vadonut", description: 'Test'}, 'dishes', (result)=> {
+    dboper.insertDocument(db, {name: "Vadonut", description: 'Test'}, 'dishes')
+    .then((result)=> {
         console.log('Insert Document:\n', result.ops); 
-
-        dboper.findDocuments(db, 'dishes', (docs) => {
+        return dboper.findDocuments(db, 'dishes')
+    })
+        .then((docs) => {
             console.log('Found documents:\n', docs);
-            
-            dboper.updateDocument(db, {name: 'Vadonut'}, {description: 'Updated test'}, 'dishes', (result)=> {
+            return dboper.updateDocument(db, {name: 'Vadonut'}, {description: 'Updated test'}, 'dishes') 
+        }) 
+        .then((result)=> {
                 console.log('Updated document:\n', result.result);
-                dboper.findDocuments(db, 'dishes', (docs)=> {
-                    console.log('Updated document:\n', docs);
+               return dboper.findDocuments(db, 'dishes')
+        })               
+              .then((docs)=> {
+                    console.log('Found document:\n', docs);
 
-                    db.dropCollection('dishes', (result)=> {
+                  return  db.dropCollection('dishes')
+              })
+                  .then((result)=> {
                         console.log('Dropped Collection: ', result); 
-
                         client.close(); 
-                    });
-                });      
-            });
-        });
-    });
- 
-});
+    })
+    .catch((err)=>  console.log(err));
+})
+.catch((err)=>  console.log(err));
